e6456f0faecea7e9e872474af2c7e3503a777302
Nathaniel B Price <nprice3@unl.edu>
2019-05-22 16:26:09 -0500

    Update to 2018 data

--- create_geodatabase.py
+++ create_geodatabase.py
@@ -1,8 +1,12 @@
-# Merge 2016 US Census addrfeat shapefiles into a geodatabase
+# Merge 2018 US Census addrfeat shapefiles into a geodatabase
 #
 # Author: Nathaniel Price
-# Date: June 14, 2017
-
+# Date: May 22, 2018
+#
+# NOTE:
+# Excute script from within ArcMap
+# execfile('C://Users//nprice3//PycharmProjects//merge_shapefiles//create_geodatabase.py')
+#
 # Setup ---------------------------------------------------------------------------------------------------------------
 # Import modules
 import arcpy
@@ -11,20 +15,20 @@ import glob
 import os
 
 # Set working directory
-os.chdir("E:\\QGIS\\tl_2016_addrfeat\\")
+os.chdir("E:\\QGIS\\tl_2018_addrfeat\\")
 
 # Create geodatabase --------------------------------------------------------------------------------------------------
 # Get list of shapefiles
 fileList = glob.glob("*.shp")
 
 # Create geodatabase
-arcpy.CreateFileGDB_management("E:/QGIS/2016_ADDRFEAT", "2016_ADDRFEAT.gdb")
+arcpy.CreateFileGDB_management("E:/QGIS/2018_ADDRFEAT", "2018_ADDRFEAT.gdb")
 
 # Add shapefiles to geodatabase
-arcpy.FeatureClassToGeodatabase_conversion(fileList, "E:/QGIS/2016_ADDRFEAT/2016_ADDRFEAT.gdb")
+arcpy.FeatureClassToGeodatabase_conversion(fileList, "E:/QGIS/2018_ADDRFEAT/2018_ADDRFEAT.gdb")
 
 # Set environment to geodatabase
-arcpy.env.workspace = r"E:/QGIS/2016_ADDRFEAT/2016_ADDRFEAT.gdb"
+arcpy.env.workspace = r"E:/QGIS/2018_ADDRFEAT/2018_ADDRFEAT.gdb"
 
 # Get list of all feature classes
 features = arcpy.ListFeatureClasses('*','All')
