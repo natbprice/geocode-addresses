90f93284335e10f93dff8add9717bbb02ed9dcce
Nathaniel B Price <nprice3@unl.edu>
2019-05-23 14:57:24 -0500

    Clean up code

--- 0_export_addresses.py
+++ 0_export_addresses.py
@@ -1,19 +1,14 @@
+# Load modules
 import pyodbc
 import csv
 import pandas as pd
 
+# Connect to SQL database
 conn = pyodbc.connect('DSN=PermitDatabase;'
                       'Database=NGPCPermits201904;'
                       'Trusted_Connection=yes;')
 
+# Save addresses to CSV in chunks
 df = pd.read_sql('SELECT * FROM Address', conn, chunksize = 1000)
 for chunk in df:
     chunk.to_csv("address.csv", encoding="utf-8", mode = "a")
-
-# cursor = conn.cursor()
-# cursor.execute('SELECT * FROM Address')
-#
-# with open("address.csv", "wb") as csv_file:
-#     csv_writer = csv.writer(csv_file)
-#     csv_writer.writerow([i[0] for i in cursor.description]) # write headers
-#     csv_writer.writerows(cursor)
